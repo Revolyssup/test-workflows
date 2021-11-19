bf2d4808dabfd3674b1d8ba82b386dc80b7250ec
ashish <ashishjaitiwari15112000@gmail.com>
2021-11-19 18:02:19 +0530

    test
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

---
+++ mesheryconf.sh
@@ -0,0 +1,6 @@
+	mkdir ~/.meshery
+	config='{"contexts":{"local":{"endpoint":"http://localhost:9081","token":"Default","platform":"kubernetes","adapters":[],"channel":"stable","version":"latest"}},"current-context":"local","tokens":[{"location":"auth.json","name":"Default"}]}'
+
+	echo $config | yq e '."contexts"."local"."adapters"[0]="'$1'"' -P - > ~/.meshery/config.yaml
+
+	cat ~/.meshery/config.yaml
