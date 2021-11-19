aaee75161ba96ad5dd417e99fabc615d684d32a0
ashish <ashishjaitiwari15112000@gmail.com>
2021-11-19 23:20:06 +0530

    test
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- configchange.sh
+++ configchange.sh
@@ -1,5 +1,5 @@
 
-	config='{"contexts":{"local":{"endpoint":"http://$ENDPOINT","token":"Default","platform":"kubernetes","adapters":[],"channel":"stable","version":"latest"}},"current-context":"local","tokens":[{"location":"auth.json","name":"Default"}]}'
+	config='{"contexts":{"local":{"endpoint":"http://'$ENDPOINT'","token":"Default","platform":"kubernetes","adapters":[],"channel":"stable","version":"latest"}},"current-context":"local","tokens":[{"location":"auth.json","name":"Default"}]}'
 
 	echo $config | yq e '."contexts"."local"."adapters"[0]="'$1'"' -P - > ~/.meshery/config.yaml
 
